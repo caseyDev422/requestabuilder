67b8714cb138d014e78c47276c9bb6331c06b67b
Sean <seancasey076@gmail.com>
2021-09-25 18:10:06 -0500

    node server connected to front end

--- requestabuilder/server/server.js
+++ requestabuilder/server/server.js
@@ -1,10 +1,25 @@
 const express = require('express');
-
+const cors = require('cors')
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 const app = express();
 const PORT = 8080;
 
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(cors());
+
+
+
 app.get('/', (req, res) => {
     console.log(__dirname);
+})
+
+app.post('/login', jsonParser, (req, res) => {
+    console.log(req.body.username);
+    console.log(req.body.password);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({message: 'Welcome'}));
 })
 
 app.listen(PORT, () => {
